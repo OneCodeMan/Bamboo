ebdc892e4c319207608284adba3e5d5208910583
Dave Gumba <dave_gumba@yahoo.com>
2016-10-15 00:30:26 -0400

    Working on deck list, could be better

---
+++ card/static/card/js/visual.js
@@ -0,0 +1,32 @@
+$hidedeck = $("#hidedeck");
+$decks = $(".deckrow");
+$hidequiz = $("#hidequiz");
+$quizzes = $(".quizrow");
+
+// toggle text
+$.fn.extend({
+	toggleText: function(a, b) {
+		return this.text(this.text() == b ? a : b);
+	}
+});
+
+$hidedeck.on('click', function() {
+	$decks.toggle("slow");
+	$(this).toggleText('hide decks', 'show decks')
+});
+
+$hidequiz.on('click', function() {
+	$quizzes.toggle("slow");
+	$(this).toggleText('hide quizzes', 'show quizzes')
+});
+
+// randomize greet
+$greet = $("#greet");
+$compliment = $("#compliment");
+greetings = ["Hello", "Hi", "Welcome"];
+compliments = ["Nice to see you again!", "Have a great time studying!", "We hope you have a great time studying :)"];
+rand_greet = Math.floor(Math.random() * greetings.length);
+rand_compliment = Math.floor(Math.random() * compliments.length);
+
+$greet.text(greetings[rand_greet]);
+$compliment.text(compliments[rand_compliment])
